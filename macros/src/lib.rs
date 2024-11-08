58060002adfb942d91e9247ca9a7af83405014b3
Jonah Lund <jonahlund@proton.me>
2024-11-08 05:27:27 +0100

    feat: lit! macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::parse_macro_input;
 use tiny_rsx::ast;
 
@@ -29,4 +30,14 @@ pub fn write(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as WriteInput);
 
     expand::write(input).into()
+}
+
+pub(crate) struct LitInput {
+    text: String,
+}
+
+#[proc_macro]
+pub fn lit(input: TokenStream) -> TokenStream {
+    let LitInput { text } = parse_macro_input!(input as LitInput);
+    quote!(::vy::PreEscaped(#text)).into()
 }
--- macros/src/parse.rs
+++ macros/src/parse.rs
@@ -1,7 +1,7 @@
 use syn::parse::{Parse, ParseStream};
 use tiny_rsx::ast::NodeTree;
 
-use crate::{LazyInput, WriteInput};
+use crate::{fmt::Formatter, LazyInput, LitInput, WriteInput};
 
 impl Parse for LazyInput {
     fn parse(input: ParseStream) -> syn::Result<Self> {
@@ -28,5 +28,29 @@ impl Parse for WriteInput {
             nodes,
             size_hint,
         })
+    }
+}
+
+impl Parse for LitInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let NodeTree { nodes } = NodeTree::parse(input)?;
+
+        let mut text = String::new();
+        let mut args = Vec::new();
+
+        let mut fmt = Formatter::new(&mut text, &mut args);
+
+        for node in &nodes {
+            let _ = fmt.write_node(node);
+        }
+
+        if let Some((_, value)) = args.into_iter().next() {
+            return Err(syn::Error::new_spanned(
+                value,
+                "only literals allowed",
+            ));
+        }
+
+        Ok(Self { text })
     }
 }
